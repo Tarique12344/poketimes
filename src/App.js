c5630c73311e0f9ca01d0a15ed9d6c89f769a4ea
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-16 13:54:19 -0500

    pushing to ghpages

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <BrowserRouter>
     <div className="App">
       <Navbar />
-      <Route exact path ="/" component={Home}/>
+      <Route exact path ="/poketimes/" component={Home}/>
       <Route path ="/about" component={About}/>
       <Route path ="/contact" component={Contact}/>
       <Route path='/:post_id' component={Post}/>
