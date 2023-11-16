906cccd1700991ce2936ce57b79d90da124f13a2
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-16 13:27:53 -0500

    projecct

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Rainbow from "../hoc/Rainbow"
+const About = () => {
+  return (
+    <div className="container">
+        <h4 className="center">about</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus consequatur veritatis, recusandae unde voluptatum nulla doloremque corrupti ratione. Distinctio, adipisci excepturi! Excepturi maxime unde aliquid quasi ex error autem minus sunt, natus, cum corrupti doloribus modi nisi ducimus, voluptatem esse! Adipisci labore recusandae impedit soluta. Amet, dolorum harum? Qui animi, harum aliquid sed enim magni labore earum excepturi mollitia quia deleniti delectus voluptas modi maxime nobis tempore, asperiores, quod fugiat nulla expedita inventore officiis fuga. Ex tempore autem vero, nisi necessitatibus eum facilis repellat dicta saepe molestias! Corrupti ipsum officia quasi saepe perspiciatis tempora veniam, corporis dolorum, suscipit modi similique accusantium dignissimos fugit laboriosam quia facilis odio excepturi. Beatae ea consequuntur sed fuga, cupiditate autem natus assumenda culpa vel eveniet inventore eos, ab reprehenderit sint, voluptatibus maiores illum voluptates laboriosam veritatis? Modi quod optio porro, quasi minus officia neque explicabo distinctio voluptate! Animi, quisquam ipsum molestias error nostrum, libero asperiores nesciunt dolor aliquid repellendus minus. Maxime nihil, animi blanditiis ut ducimus quaerat voluptas id a tempora! Praesentium dignissimos beatae repellat laborum debitis. Quidem assumenda, odit esse pariatur itaque laudantium ex saepe, quia quisquam qui cum dolorem temporibus commodi quasi ut similique aliquid. Delectus corporis, eveniet a ad reprehenderit et!</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
